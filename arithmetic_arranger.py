7afec64d6f535acc255760e8159da6e8ad11cefe
Richards-R <rr7366@gmail.com>
2023-10-05 13:19:36 +0000

    passed all test

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,23 +1,20 @@
-def arithmetic_arranger(problems, resultsDisplay):
+def arithmetic_arranger(problems, resultsDisplay=''):
 
     problemLength = len(problems)
-    if problemLength > 4:
+    if problemLength > 5:
         return "Error: Too many problems."
-    #print(problemLength)
 
     operandTop = ""
     operandLow = ""
     result = 0
     underRule = ""
+    
     operandTopRow = ""
     operandLowRow = ""
     underRuleRow = ""
     resultRow = ""
 
     for equation in problems:      
-
-        #print(equation)
-       
         operator = "+"
         equationStr = equation
         for character in equationStr:
@@ -29,24 +26,22 @@ def arithmetic_arranger(problems, resultsDisplay):
               return "Error: Operator must be '+' or '-'."
             elif character == "*":
               return "Error: Operator must be '+' or '-'."
-        #print(operator)
-               
+                       
         operatorPosition = equationStr.find(operator)
-        #print(operatorPosition)
-    
         operandTop = equationStr[:operatorPosition].strip()
         operandLow = equationStr[operatorPosition +1:].strip()       
-        #print(operandTop)
-        #print(operandLow)
-      
-        
+            
+        if operandTop.isnumeric() == False:
+          return "Error: Numbers must only contain digits."
+        if operandLow.isnumeric() == False:
+          return "Error: Numbers must only contain digits."
+              
         if resultsDisplay == True:
             if operator == "+":
                 result = int(operandTop) + int(operandLow)
             elif operator == "-":
                 result = int(operandTop) - int(operandLow)
-        
-        
+                   
         if int(result) < 0:
           result = "   " + str(result)
         else:
@@ -56,8 +51,8 @@ def arithmetic_arranger(problems, resultsDisplay):
 
         longestOperandLength = 0
         diff = len(operandTop) - len(operandLow)
-        #print(diff)
-        if diff > 0:
+    
+        if diff >= 0:
             longestOperandLength = len(operandTop)
             while diff > 0:
                 operandLow = " " + operandLow
@@ -68,41 +63,36 @@ def arithmetic_arranger(problems, resultsDisplay):
             while diff*-1 > 0:
                 operandTop = " " + operandTop
                 diff = diff + 1
+
+        if longestOperandLength > 4:
+            return "Error: Numbers cannot be more than four digits."        
         
-        operandTop = "    " + operandTop
-        operandLow = "  " + operator + " " + operandLow
-    
+        underRuleLength = 0
+        underRuleLength = longestOperandLength + 2
+        while underRuleLength > 0:
+            underRule = underRule + "-"
+            underRuleLength = underRuleLength - 1 
         
-        #while longestOperandLength > 0:
-            #underRule = underRule + "-"
-            #longestOperandLength = longestOperandLength - 1
+        underRule = underRule + "    "
         
-        # print(operandTop)
-        #print(operandLow)
-        #print(underRule)
-        #if resultsDisplay == True:
-            #print(resultRow)
-    
-        #print(problems)
-    
+        operandTop = "      " + operandTop
+        operandLow = "    " + operator + " " + operandLow
         
         operandTopRow = operandTopRow + operandTop
         operandLowRow = operandLowRow + operandLow
-                  
-        resultRow = resultRow + str(result)
 
-    # print(operandTopRow)
-    # print(operandLowRow)
-    underRuleLength = len(operandTopRow)
-    while underRuleLength > 0:
-        underRuleRow = underRuleRow + "-"
-        underRuleLength = underRuleLength - 1
-    # print(underRuleRow)
-    # if resultsDisplay == True:
-    #     print(resultRow)
-   
-
-    if resultsDisplay == True:
-       return operandTopRow + "\n" + operandLowRow + "\n" + underRuleRow + "\n" + resultRow
-    else: 
-       return operandTopRow + "\n" + operandLowRow + "\n" + underRuleRow
+        if len(result.strip()) > longestOperandLength:
+            if int(result) < 0:
+              resultRow = resultRow + "  " + str(result)
+            else:
+              resultRow = resultRow + " " + str(result)             
+        elif len(result.strip()) <= longestOperandLength: 
+            resultRow = resultRow + "  " + str(result)
+              
+    underRuleRow =  underRuleRow + underRule       
+    
+    if resultsDisplay == "":
+      output = operandTopRow[4:] + "\n" + operandLowRow[4:] + "\n" + underRuleRow.rstrip()
+    elif resultsDisplay == True:
+      output = operandTopRow[4:] + "\n" + operandLowRow[4:] + "\n" + underRuleRow.rstrip() + "\n" + resultRow[4:]
+    return output 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pytest import main
 from arithmetic_arranger import arithmetic_arranger
 
 
-print(arithmetic_arranger(["912 + 68", "1 - 3801", "45 + 43", "123 + 49"],True))
+print(arithmetic_arranger(['9998 + 9999', '32 - 698', '1 - 3801', '45 + 43', '123 + 49' ], True))
 
 
 # Run unit tests automatically
